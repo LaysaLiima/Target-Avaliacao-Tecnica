4de4e672793082f55e6464b9cdb85abee0e86945
Laysa Liima <142900878+LaysaLiima@users.noreply.github.com>
2024-03-03 13:58:19 -0300

    feat: adiciona arquivo Percentual_mensal.py
    Este programa calcula o percentual de representação que cada estado teve dentro do valor total mensal de uma distribuidora, com base nos valores de faturamento mensal detalhados por estado fornecidos. Ele utiliza esses valores para calcular a porcentagem que cada estado representa em relação ao total mensal da distribuidora.

---
+++ Percentual_mensal.py
@@ -0,0 +1,19 @@
+faturamento_SP = 67836.43
+faturamento_RJ = 36678.66 
+faturamento_MG = 29229.88
+faturamento_ES = 27165.48
+faturamento_Outros = 1949.53
+
+valor_total = faturamento_SP  + faturamento_RJ + faturamento_MG + faturamento_ES + faturamento_Outros
+
+porcentagem_SP = (67836.43 / 180759.98)* 100;
+porcentagem_RJ =(36678.66 / 180759.98)* 100;
+porcentagem_MG = (29229.88 / 180759.98)* 100;
+porcentagem_ES = (27165.48 / 180759.98)* 100;
+porcentagem_Outros = (1949.53 / 180759.98)* 100;
+
+print(f"O percentual do estado de São Paulo foi  {porcentagem_SP:.2f}%" )
+print(f"O percentual do estado de Rio de Janeiro  foi {porcentagem_RJ:.2f}% ")
+print(f"O percentual do estado de Minas Gerais foi  {porcentagem_MG:.2f}%")
+print(f"O percentual do estado de Espirito Santo foi  {porcentagem_ES:.2f}%");
+print(f"O percentual do estado de Outros foi  {porcentagem_Outros:.2f}%");		  
