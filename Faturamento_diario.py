9632a730b8739a855c2c03cb255a09d964ae33a6
Laysa Liima <142900878+LaysaLiima@users.noreply.github.com>
2024-03-03 13:56:54 -0300

    feat : adiciona Faturamento_diario.py e arquivo.json
    Este programa utiliza um arquivo JSON contendo os valores diários de faturamento de uma distribuidora para calcular e retornar o menor e o maior valor de faturamento ocorrido em um dia do mês, bem como o número de dias no mês em que o faturamento diário foi superior à média mensal, ignorando os dias sem faturamento, como fins de semana e feriados.

---
+++ Faturamento_diario.py
@@ -0,0 +1,27 @@
+import json
+
+with open('arquivo.json', 'r') as arquivo:
+    dados_faturamento = json.load(arquivo)
+print(dados_faturamento)  
+
+valores_diarios = [dado['valor'] for dado in dados_faturamento]
+
+faturamento_max = max(valores_diarios)
+faturamento_min = min(valores_diarios)
+
+print(f"O maior valor de faturamento ocorrido em um dia do mês foi de: {faturamento_max}")
+print(f"O menor valor de faturamento ocorrido em um dia do mês foi de: {faturamento_min}")
+
+soma_dos_valores_diarios = 0
+
+for valor in valores_diarios:
+    soma_dos_valores_diarios += valor
+
+media_mensal = soma_dos_valores_diarios / 21
+
+count = 0
+
+for valor in valores_diarios:
+    if valor > media_mensal:
+        count += 1
+print(f"Número de dias no mês em que o valor de faturamento diário foi superior à média mensal: {count}")
