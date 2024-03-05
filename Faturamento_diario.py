625e94a34d335ff8cd810928dec62408b7d9cea2
Laysa Liima <142900878+LaysaLiima@users.noreply.github.com>
2024-03-04 23:37:47 -0300

    feat(Faturamento_mensal): Armazena e exibe o dia junto com menor e maior valores
    Implementa a funcionalidade de exibir os dias correspondente aos valores mínimo e máximo encontrados.

--- Faturamento_diario.py
+++ Faturamento_diario.py
@@ -2,26 +2,32 @@ import json
 
 with open('arquivo.json', 'r') as arquivo:
     dados_faturamento = json.load(arquivo)
-print(dados_faturamento)  
 
-valores_diarios = [dado['valor'] for dado in dados_faturamento if dado.get('valor', 0)!=0]
+valores_diarios = [(dado['dia'], dado['valor']) for dado in dados_faturamento if dado.get('valor', 0) != 0]
 
-faturamento_max = max(valores_diarios)
-faturamento_min = min(valores_diarios)
+dia_min, valor_min = valores_diarios[0]
+for dia,valor in valores_diarios:
+    if valor < valor_min:
+        valor_min = valor 
+        dia_min = dia
 
-print(f"O maior valor de faturamento ocorrido em um dia do mês foi de: {faturamento_max}")
-print(f"O menor valor de faturamento ocorrido em um dia do mês foi de: {faturamento_min}")
+dia_max, valor_max = valores_diarios[0]
+for dia,valor in valores_diarios:
+    if valor > valor_max:
+        valor_max = valor 
+        dia_max = dia
+
+print(f"O maior valor diário foi de R${valor_max:.2f}, no dia {dia_max}")
+print(f"O menor valor diário foi de R${valor_min:.2f}, no dia {dia_min}")
 
 soma_dos_valores_diarios = 0
-
-for valor in valores_diarios:
+for dia, valor in valores_diarios:
     soma_dos_valores_diarios += valor
 
 media_mensal = soma_dos_valores_diarios / len(valores_diarios)
 
 count = 0
-
-for valor in valores_diarios:
+for dia, valor in valores_diarios:
     if valor > media_mensal:
         count += 1
 print(f"Número de dias no mês em que o valor de faturamento diário foi superior à média mensal: {count}")
